c3bb49a50ca8ff4051f6f70992fb0690dfd46cec
Gallahad072 <gallahad072@gmail.com>
2021-10-26 09:23:27 +0100

    reformat

--- open_random_pic.py
+++ open_random_pic.py
@@ -2,17 +2,20 @@ import webbrowser
 import random
 import string
 
+
 def prnt():
-  text = ''.join(random.choices(string.ascii_lowercase, k=2))
-  ints = random.randint(0, 10000)
-  url = f'www.prnt.sc/{text}{ints:04d}'
-  webbrowser.open(url)
+    text = "".join(random.choices(string.ascii_lowercase, k=2))
+    ints = random.randint(0, 10000)
+    url = f"www.prnt.sc/{text}{ints:04d}"
+    webbrowser.open(url)
+
 
 def imgur():
-  text = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
-  url = f'www.imgur.com/{text}'
-  webbrowser.open(url)
+    text = "".join(random.choices(string.ascii_letters + string.digits, k=5))
+    url = f"www.imgur.com/{text}"
+    webbrowser.open(url)
+
 
 for i in range(10):
-  prnt()
-  imgur()
+    prnt()
+    # imgur()
