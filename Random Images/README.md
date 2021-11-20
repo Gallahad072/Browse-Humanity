77b3f6c451fd972bbf0578dc9b2c409e32be3215
Gallahad072 <gallahad072@gmail.com>
2021-11-20 19:26:11 +0000

    commented and readme

---
+++ Random Images/README.md
@@ -0,0 +1,39 @@
+# Random Image Generator
+
+Welcome to another fun waste of time.
+
+This will open random images from imgur in your browser.
+
+<span style="color:red">**_!!! WARNING: Images may be NSFW, must be 18+ !!!_**</span>
+
+## Instructions
+
+You can use it two ways:
+
+---
+
+**Run the python file and use the UI**
+
+Type in the terminal:
+
+`python main.py`
+
+The UI will then prompt an input.
+
+```
+'q' to quit
+
+ Number of random images: 20
+```
+
+Replace '20' with however many images you want.
+
+---
+
+**Put the number of images you want to open as an argument in your terminal**
+
+Type in the terminal:
+
+`python main.py 20`
+
+Replace '20' with however many images you want
--- Random Images/main.py
+++ Random Images/main.py
@@ -1,3 +1,4 @@
+import sys
 import webbrowser
 import random
 import string
@@ -6,31 +7,25 @@ from PIL import Image
 from io import BytesIO
 
 
+# functions that finds and open images
 def getImages(number_of_images):
     valid_urls = set()
     while len(valid_urls) != number_of_images:
         text = "".join(random.choices(string.ascii_letters + string.digits, k=5))
         url = f"https://www.imgur.com/{text}.png"
         img = Image.open(BytesIO(requests.get(url).content))
+        # filters out images with a width or hieght less than 100
         if (img.width and img.height) > 100:
             valid_urls.add(url)
 
+    # opens all images in one batch
     for url in valid_urls:
         webbrowser.open_new(url)
 
 
+# ui to receive input in the console
 def main():
-    print(
-        """
-Welcome to another fun waste of time.
-
-This will open random images from imgur in your browser
-    
-    !!! WARNING: Images may be NSFW !!!
-
-'q' to quit
-"""
-    )
+    print("\n'q' to quit\n")
     while True:
         inpt = input("Number of random images: ")
         if inpt == "q":
@@ -38,8 +33,15 @@ This will open random images from imgur in your browser
         if inpt.isdigit():
             getImages(int(inpt))
         else:
-            print("\n    ERROR: Enter a valid value\n")
+            print("\n\TypeError: Value not digit\n")
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        inpt = sys.argv[1]
+        if inpt.isdigit():
+            getImages(int(inpt))
+        else:
+            print("\n\TypeError: Value not digit\n")
+    else:
+        main()
