8ecb8ba697f313ec405de07bc587749937d3daff
Gallahad072 <gallahad072@gmail.com>
2021-11-21 13:10:41 +0000

    added loading screen

--- Random Image Generator/main.py
+++ Random Image Generator/main.py
@@ -5,10 +5,23 @@ import string
 import requests
 from PIL import Image
 from io import BytesIO
+import curses
+
+
+# Initializes loading screen
+def loading_screen(stdscr, number_of_images):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_GREEN)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    stdscr.clear()
+    stdscr.addstr("Loading Images ...\n")
+    stdscr.addstr("#" * 50, curses.color_pair(2))
+    stdscr.addstr(f"\n0/{number_of_images}")
+    stdscr.refresh()
+    getImages(number_of_images, stdscr=stdscr)
 
 
 # functions that finds and open images
-def getImages(number_of_images):
+def getImages(number_of_images, stdscr=False):
     valid_urls = set()
     while len(valid_urls) != number_of_images:
         text = "".join(random.choices(string.ascii_letters + string.digits, k=5))
@@ -18,20 +31,29 @@ def getImages(number_of_images):
         if (img.width and img.height) > 100:
             valid_urls.add(url)
 
+            if stdscr:
+                points = "#" * round(len(valid_urls) / number_of_images * 50)
+                stdscr.addstr(1, 0, f"{points}", curses.color_pair(1))
+                stdscr.addstr(2, 0, f"{len(valid_urls)}/{number_of_images}")
+                stdscr.refresh()
+
     # opens all images in one batch
     for url in valid_urls:
         webbrowser.open_new(url)
 
 
 # ui to receive input in the console
-def main():
+def main(progress_bar=True):
     print("\n'q' to quit\n")
     while True:
         inpt = input("Number of random images: ")
         if inpt == "q":
             break
         if inpt.isdigit():
-            getImages(int(inpt))
+            if progress_bar:
+                curses.wrapper(loading_screen, int(inpt))
+            else:
+                getImages(int(inpt))
         else:
             print("\n\TypeError: Value not digit\n")
 
@@ -40,7 +62,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         inpt = sys.argv[1]
         if inpt.isdigit():
-            getImages(int(inpt))
+            curses.wrapper(loading_screen, int(inpt))
         else:
             print("\n\TypeError: Value not digit\n")
     else:
